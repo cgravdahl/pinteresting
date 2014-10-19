cd026b9fbef2324bb58359d1d122caa829ed81f0
Cody Gravdahl <codygravdahl@gmail.com>
2014-10-19 14:40:54 -0500

    Fixed error in Pins Contorller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,12 +6,10 @@ class PinsController < ApplicationController
   end
 
   def show
-  
   end
 
   def new
     @pin = Pin.new
-    
   end
 
   def edit
@@ -20,29 +18,32 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
-      redirect_to pins_path, notice: 'Pin was successfully created.'
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render action: 'new'
-
+    end
   end
 
   def update
     if @pin.update(pin_params)
-      redirect_to pins_path, notice: 'Pin was successfully updated.'
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
       render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_path
+    redirect_to pins_url
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
